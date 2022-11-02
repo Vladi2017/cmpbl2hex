efe9bffb20e39a9cda8f15fb351fe484e0ad3392
VladiManolescu <mvmanol@yahoo.com>
2022-11-02 18:58:22 +0200

    OK!! but still we have a little bug, usage under Cygwin:
    vladi@VladiLaptop1W10 /cygdrive/c/Users/mvman/Downloads/tmp1/tmp1/tmp1/tmp/png2svg
    $ cmpbl2hex --help
    Intented usage:
     $ cmp -bl file1 file2 | cmpbl2hex
    vladi@VladiLaptop1W10 /cygdrive/c/Users/mvman/Downloads/tmp1/tmp1/tmp1/tmp/png2svg
    $ cmp -bl fc2Id0U8.png fc2Id0U8_2.png
    43899 316 M-N  126 V
    vladi@VladiLaptop1W10 /cygdrive/c/Users/mvman/Downloads/tmp1/tmp1/tmp1/tmp/png2svg
    $ cmp -bl fc2Id0U8.png fc2Id0U8_2.png|cmpbl2hex
    ab7a ce M-N  56 V
    
    But now:
    vladi@VladiLaptop1W10 /cygdrive/c/Users/mvman/Downloads/tmp1/tmp1/tmp1/tmp/png2svg
    $ cmp -bl fc2Id0U8.png fc2Id0U8_2.png
    33418 162 r    347 M-g
    33505  63 3    167 w
    
    vladi@VladiLaptop1W10 /cygdrive/c/Users/mvman/Downloads/tmp1/tmp1/tmp1/tmp/png2svg
    $ cmp -bl fc2Id0U8.png fc2Id0U8_2.png|cmpbl2hex
    8289 72 r    e7 M-g  #{correct}
    82e0  33 3    167 w  #{here we should have: "82e0  33 3  77 w"}

--- cmpbl.py
+++ cmpbl.py
@@ -1,7 +1,17 @@
 #!python
 import sys
-if sys.argv.__len__() == 2: x = open(sys.argv[1], 'rt', encoding='ascii')
+import re   # there is not installed regex module under Cygwin
+usage = 'Intented usage:\n $ cmp -bl file1 file2 | cmpbl2hex'
+reo1 = re.compile(r'^(\d+)(\s+)(\d+)([^\d]+)(\d+)(.*)')
+if sys.argv.__len__() == 2:
+    if sys.argv[1] == '-h' or sys.argv[1] == '--help':
+        print(usage)
+        exit()
+    else: x = open(sys.argv[1], 'rt', encoding='ascii')
 else: x = sys.stdin
 while line := x.readline():  # https://docs.python.org/3.8/reference/expressions.html#assignment-expressions
-        line = line.rstrip('\r\n')
-        print(line, '+Vld.')
+    line = line.rstrip('\r\n')
+    match = re.match(reo1, line)
+    addr, val1, val2 = int(match.group(1)) - 1, int(match.group(3), 8), int(match.group(5), 8)
+    lineHex = "{0:x}{1}{2:x}{3}{4:x}{5}".format(addr, match.group(2), val1, match.group(4), val2, match.group(6))
+    print(lineHex)
