7a07f95eb87a88b1210d4493c3f6ace6b24623c5
VladiManolescu <mvmanol@yahoo.com>
2022-11-02 03:38:37 +0200

    Now is OK
    Read from file OR from stdin.:
    vladi@VladiLaptop1W10 /cygdrive/c/Users/mvman/VladiDocuments/MAI_VladiLaptopWXP/MyDocuments/stuff
    $ cmpbl2hex t1.txt
    unu +Vld.
    doi +Vld.
    ls +Vld.
    trei +Vld.
    patru +Vld.
    
    vladi@VladiLaptop1W10 /cygdrive/c/Users/mvman/VladiDocuments/MAI_VladiLaptopWXP/MyDocuments/stuff
    $
    
    vladi@VladiLaptop1W10 /cygdrive/c/Users/mvman/VladiDocuments/MAI_VladiLaptopWXP/MyDocuments/stuff
    $ echo bau|cmpbl2hex
    bau +Vld.

--- cmpbl.py
+++ cmpbl.py
@@ -1,8 +1,7 @@
 #!python
 import sys
-if sys.argv.__len__() == 2: inFile = sys.argv[1]
-else: inFile = sys.stdin
-with open(inFile, 'rt', encoding='ascii') as x:
-    while line := x.readline():  # https://docs.python.org/3.8/reference/expressions.html#assignment-expressions
+if sys.argv.__len__() == 2: x = open(sys.argv[1], 'rt', encoding='ascii')
+else: x = sys.stdin
+while line := x.readline():  # https://docs.python.org/3.8/reference/expressions.html#assignment-expressions
         line = line.rstrip('\r\n')
         print(line, '+Vld.')
