716c4ac1683fb23a3d7e484a8f5b6a9ee65be532
VladiManolescu <mvmanol@yahoo.com>
2022-11-02 03:32:22 +0200

    We have errors on stdin case:
    vladi@VladiLaptop1W10 /cygdrive/c/Users/mvman/VladiDocuments/MAI_VladiLaptopWXP/MyDocuments/stuff
    $ cmpbl2hex t1.txt
    unu +Vld.
    doi +Vld.
    ls +Vld.
    trei +Vld.
    patru +Vld.
    
    vladi@VladiLaptop1W10 /cygdrive/c/Users/mvman/VladiDocuments/MAI_VladiLaptopWXP/MyDocuments/stuff
    $ echo bau|cmpbl2hex
    Traceback (most recent call last):
      File "/usr/local/bin/cmpbl2hex", line 5, in <module>
        with open(inFile, 'rt', encoding='ascii') as x:
    TypeError: expected str, bytes or os.PathLike object, not TextIOWrapper

--- cmpbl.py
+++ cmpbl.py
@@ -1,6 +1,7 @@
 #!python
 import sys
-inFile = sys.argv[1]
+if sys.argv.__len__() == 2: inFile = sys.argv[1]
+else: inFile = sys.stdin
 with open(inFile, 'rt', encoding='ascii') as x:
     while line := x.readline():  # https://docs.python.org/3.8/reference/expressions.html#assignment-expressions
         line = line.rstrip('\r\n')
